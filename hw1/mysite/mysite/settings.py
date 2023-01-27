1c3b1a2b0f33b4cb74410b61bb5c5f347894d7a8
WalterJ726 <walterjiang726@gmail.com>
2023-01-26 23:06:05 -0500

    learn django part1-4

--- hw1/mysite/mysite/settings.py
+++ hw1/mysite/mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'sharecar.apps.SharecarConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
--- hw1/mysite/polls/apps.py
+++ hw1/mysite/polls/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SharecarConfig(AppConfig):
+class PollsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sharecar'
+    name = 'polls'
